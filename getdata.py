687afc219f697085c0b84b7ea2fd948e7f167f48
Richard Barrett-Small <rickbs@gmail.com>
2015-07-16 22:33:29 +0100

    Counting TRs

--- getdata.py
+++ getdata.py
@@ -2,9 +2,10 @@ import sys
 from bs4 import BeautifulSoup
 
 if len(sys.argv) > 1:
-
     fn = sys.argv[1]
     f = open(fn)
     html = f.read()
     soup = BeautifulSoup(html)
-    print [len(s.prettify()) for s in soup.find_all('table')]
+    table_one = soup.find_all('table')[0]
+    rows = table_one.find_all('tr')
+    print len(rows)
